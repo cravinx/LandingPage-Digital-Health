1f478d5167464541b585f5b6a953683b4027af44
cravinx <unegbuprecious25@gmail.com>
2024-10-09 15:54:55 +0200

    styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {
-      backgroundImage: {
-        'hero-pattern': "url('../Images/DHlogo.png')",
-        'hero-pattern-2': "url('../Images/DHbg.jpg')",
-        'footer-texture': "url('/img/footer-texture.png')",
-      }
     },
   },
   plugins: [],
